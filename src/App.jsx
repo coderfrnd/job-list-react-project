663b4da4c3cd0f6283338867f878d2ecd0892949
Abhishek <warriorsshadow39@gmail.com>
2025-04-06 00:15:00 +0530

    adding seprate backend server

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,72 @@
-import { useState } from 'react'
-import {Route,Routes} from 'react-router'
-import Home from './Layout/Home'
-import Jobs from './Layout/Jobs'
-import Addjobs from './Layout/Addjobs'
-import JobPage from './component/JobPage'
-import Editjob from './component/Editjob'
+import { useState } from "react";
+import { Route, Routes } from "react-router";
+import Home from "./Layout/Home";
+import Jobs from "./Layout/Jobs";
+import Addjobs from "./Layout/Addjobs";
+import JobPage from "./component/JobPage";
+import Editjob from "./component/Editjob";
 
 function App() {
- 
-  async function addJob(formData){
-    const res = await fetch("http://localhost:8000/jobs" , {
-      method: 'POST',
-      headers:{
-        'Content-Type':"application/json"
+  async function addJob(formData) {
+    const res = await fetch("https://job-list-server-3.onrender.com/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify(formData)
-    })
-    const data = await res.json()
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
     console.log(data);
-    return
-   }
-async function deleteFormData(jobId){
-  const res = await fetch(`http://localhost:8000/jobs/${jobId}`, {
-    method: 'DELETE',
-  })
-  alert("Deleted succeesfully")
-}
-async function editFormData(updatedFormData) {
-  try {
-    let {id} = updatedFormData
-  const res = await fetch(`http://localhost:8000/jobs/${id}` , {
-    method: 'PUT',
-    headers:{
-      'Content-Type':"application/json"
-    },
-    body:JSON.stringify(updatedFormData)
-  })
-  const data = await res.json()
-  console.log(data);
-  } catch (error) {
-    console.log("Error in  app page updated section",error);
+    return;
   }
-  
-  return
-}
+  async function deleteFormData(jobId) {
+    const res = await fetch(
+      `https://job-list-server-3.onrender.com/jobs/${jobId}`,
+      {
+        method: "DELETE",
+      }
+    );
+    alert("Deleted succeesfully");
+  }
+  async function editFormData(updatedFormData) {
+    try {
+      let { id } = updatedFormData;
+      const res = await fetch(
+        `https://job-list-server-3.onrender.com/jobs/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedFormData),
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log("Error in  app page updated section", error);
+    }
+
+    return;
+  }
 
   return (
     <>
-   <Routes>
-    <Route index element = {<Home/>} />
-    <Route path='/jobs' element = {<Jobs/>} />
-    <Route path='/jobs/:id' element = {<JobPage deleteFormData={deleteFormData} />} />
-    <Route path='/edit-jobs/:id' element = {<Editjob addJobUpdatedSubmit = {editFormData} />} />
-    <Route path='/addjobs' element= {<Addjobs addJobSubmit = {addJob} />}/>
-   </Routes>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteFormData={deleteFormData} />}
+        />
+        <Route
+          path="/edit-jobs/:id"
+          element={<Editjob addJobUpdatedSubmit={editFormData} />}
+        />
+        <Route path="/addjobs" element={<Addjobs addJobSubmit={addJob} />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Joblist.jsx
+++ src/component/Joblist.jsx
@@ -1,49 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import Jobcard from './Jobcard'
-import Spinner from './Assets/Cliploader'
-const Joblist = ({isHome = false}) => {  
-    const [jobs,setJobs] = useState([])
-    const [loading , setLoading] = useState(true)
-    useEffect(()=>{
-      async function fetchData (){
-        let url = isHome ? "?_limit=3" : "";
-        let apiUrl = `http://localhost:8000/jobs/`+url
-        console.log(apiUrl);    
+import React, { useEffect, useState } from "react";
+import Jobcard from "./Jobcard";
+import Spinner from "./Assets/Cliploader";
+const Joblist = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchData() {
+      let url = isHome ? "?_limit=3" : "";
+      let apiUrl = `https://job-list-server-3.onrender.com/jobs/` + url;
+      console.log(apiUrl);
       try {
-          let res = await fetch(apiUrl)
-          let data = await res.json()
-           setJobs(data)
-       
+        let res = await fetch(apiUrl);
+        let data = await res.json();
+        setJobs(data);
       } catch (error) {
-        console.log("Errors in Job List Section",error);
-      }  finally{
-      setLoading(false)
+        console.log("Errors in Job List Section", error);
+      } finally {
+        setLoading(false);
       }
-    } 
-            fetchData()
-    },[])
+    }
+    fetchData();
+  }, []);
   return (
-   <>
-   <section className="bg-blue-50 px-4 py-10">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        {isHome ? "Recent Job" : "Browse Job"}
-        </h2>
-        {loading ? (
-  <div className="flex justify-center items-center h-40">
-    <Spinner loading={loading} />
-  </div>
-) : (
-  <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-    {jobs.map((ele) => (
-      <Jobcard key={ele.id} {...ele} />
-    ))}
-  </div>
-)}
-      </div>
-    </section>   
-   </>
-  )
-}
+    <>
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            {isHome ? "Recent Job" : "Browse Job"}
+          </h2>
+          {loading ? (
+            <div className="flex justify-center items-center h-40">
+              <Spinner loading={loading} />
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((ele) => (
+                <Jobcard key={ele.id} {...ele} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Joblist
+export default Joblist;
